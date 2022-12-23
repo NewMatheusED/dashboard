7a4cc1326e29e72139234ed2534e8da9f58dd7ed
NewMatheusED <58958905+NewMatheusED@users.noreply.github.com>
2022-12-23 15:36:15 -0300

    .

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sideBar">
+        <div class="container">
+            <div class="headerSide">
+                <h3>RS</h3>
+            </div><!--headerSide-->
+            <div class="menuSide">
+                <ul>
+                    <li><i class="fa-solid fa-house-user"></i><a>Home</a></li>
+                    <li><i class="fa-solid fa-house-user"></i><a>Home</a></li>
+                    <li><i class="fa-solid fa-house-user"></i><a>Home</a></li>
+                    <li><i class="fa-solid fa-house-user"></i><a>Home</a></li>
+                </ul>
+            </div>
+        </div><!--container-->
+    </div><!--sideBar-->
+
+    <div class="mainContent">
+        <div class="container">
+            <header>
+                <div class="alertIcon">
+                    <i class="fa-solid fa-bell"></i>
+                    <span>2</span>
+                </div><!--alertIcon-->
+
+                <div class="campoPesquisa">
+                    <form>
+                        <input type="text">
+                    </form>
+                    <div class="icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+                </div><!--campoPesquisa-->
+            </header>
+
+            <div class="flexContent">
+
+                <div class="content">
+                    <div class="panel">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+
+                    <div class="panel w50">
+                        <h1>Olá User, seja bem vindo!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veritatis sequi dignissimos sapiente similique quo aperiam iusto! Doloremque cum voluptatem magni! Asperiores molestiae dolor minima enim, vel quis? Asperiores, quae?</p>
+                    </div><!--panel-->
+                </div><!--content-->
+
+
+                <div class="notification">
+                    <div class="notificationHeader">
+                        <h2>Últimas Atividades</h2>
+                    </div><!--notificationHeader-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                    <div class="notificationContent">
+                        <h2>Há 2 minutos</h2>
+                        <h1>Nova paágina inserida</h1>
+                        <p>Lorem ipsun dolor sit amet consequent dolores</p>
+                    </div><!--notificationContent-->
+
+                </div><!--notification-->
+
+            </div><!--flexContent-->
+
+        </div><!--container-->
+    </div><!--mainContent-->
+    
+    <script src="app.js"></script>
+</body>
+</html>
